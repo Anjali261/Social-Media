fc12eb18fbf7cd102022c8bcc49b8210f4d7ddc5
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-06-27 12:10:00 +0530

    Setting up backend with user Model

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+
+//helmet help us to protect the express server by setting up various http headers. 
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const helmet = require("helmet");
+const morgan = require('morgan');
+require('dotenv').config();
+const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
+
+
+//The express.json() function is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser. 
+
+//middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+mongoose.set('strictQuery', false);
+const connectDB = async() =>{
+    try{
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+
+        })
+        console.log(`MongoDB connected: ${con.connection.host}`);
+        
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+
+    }
+}
+connectDB();
+
+
+app.use("/api/users",userRoute)
+app.use("/api/auth",authRoute)
+
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Server is listening at http://localhost:${process.env.PORT}`);
+})
+
+
