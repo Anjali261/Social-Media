a8db3e53f1a3e9910a5ad8024c1fc904b9696131
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-06-29 04:28:32 +0530

    Required Dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import Home from "./pages/home/Home";
 import {Person} from "@material-ui/icons"
 import Profile from "./pages/profile/Profile";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
-    // <Home />
-    <Profile />
+    <Home />
+    // <Profile />
+    // <Login />
+    // <Register />
   );
 }
 
